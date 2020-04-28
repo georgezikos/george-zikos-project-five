263e10e8ca306f10ab1e763b4cbe706bedeabe87
georgezikos <george.zikos@icloud.com>
2020-04-28 14:48:36 -0400

    Updates

---
+++ precache-manifest.ace29713726fc2721196080f2e9bd86b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846112ff7ac89e524a0031c6977d8742",
+    "url": "/george-zikos-project-five/index.html"
+  },
+  {
+    "revision": "782623613cfdcc150797",
+    "url": "/george-zikos-project-five/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e53ce62056a3c005f8d",
+    "url": "/george-zikos-project-five/static/js/2.e274a90d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/george-zikos-project-five/static/js/2.e274a90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782623613cfdcc150797",
+    "url": "/george-zikos-project-five/static/js/main.3c51a4ff.chunk.js"
+  },
+  {
+    "revision": "4eaa711dd7752760b67d",
+    "url": "/george-zikos-project-five/static/js/runtime-main.cc0bd6a1.js"
+  }
+]);
